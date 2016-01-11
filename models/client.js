3e7e03f4872fc3323fad46a1ed4dca365e4be2db
Jérémy De Sousa <jeremy.de-sousa@etu.univ-lyon1.fr>
2016-01-11 11:52:15 +0100

    Add client model

---
+++ models/client.js
@@ -0,0 +1,28 @@
+"use strict";
+
+module.exports = function(sequelize, DataTypes) {
+    var Client = sequelize.define("Client", {
+        id: {
+            type :  DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        nom: DataTypes.STRING(45),
+        prenom: DataTypes.STRING(45),
+        date_naissance: DataTypes.DATE,
+    }, {
+        timestamps: false,
+        freezeTableName: true, // Model tableName will be the same as the model name
+        tableName: 'client'
+        /*
+         classMethods: {
+         // ICI, on défnit les relations avec les autre models!!
+         associate: function(models) {
+         User.hasMany(models.Task)
+         }
+         }
+         */
+    });
+
+    return Client;
+};
