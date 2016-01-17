61212d2ae47e291ddb8017236918fb9416405850
sebduret <sebastienduret@msn.com>
2016-01-17 15:22:44 +0100

    add route : api/stations/station_id

--- routes/stations.js
+++ routes/stations.js
@@ -4,10 +4,23 @@ var router  = express.Router();
 
 router.get('/stations', function(req, res) {
     models.Station.findAll({
-       // attributes: ['idStation', 'adresse']
     }).then(function(data) {
         res.json({status:true,data: data});
     });
 });
+
+router.get('/stations/:station_id', function(req, res) {
+    models.Station.findOne({
+        where: {idStation: req.params.station_id},
+        include:[{model:models.Borne}]
+    }).then(function(data) {
+        if (data == null){
+            res.json({status:false,data: "Oops... Make sure you entered a valid station!"});
+        }else{
+            res.json({status:true,data: data});
+        }
+    });
+});
+
 
 module.exports = router;
